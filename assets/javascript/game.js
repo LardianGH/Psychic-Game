3d942cde900093b58ececec6765e756b4dd9f4d4
Zion Brown <zabs95@gmail.com>
2019-08-03 04:35:46 -0400

    Added quotes for the boss to say

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,49 +3,55 @@ var losses = 0;
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var guesses
 var previous
-var randomNum
+var randomLetter
+var evilQuote = ["Not even close.", "You might as well stop now.", "Are you even qualified to face me?", "What a pathetic attempt at survival", "I'm enjoying watching you flounder", "Watching you fail so bad... It almost makes me feel sorry", "Is there someone else there? someone... skilled?"]
 
 restart = function () {
-    guesses = 10;
-    previous = [];
-    randomNum = alphabet[Math.floor(Math.random() * alphabet.length)];
-    console.log(randomNum)
+    guesses = 10; //resets number of guesses to 10
+    previous = []; //clears the guesses so far
+    randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)]; //generates a new random letter
+    console.log(randomLetter) //logs the random letter
 };
 
 function enterGuess() {
 
-document.getElementById("inputBox").value;
+document.getElementById("inputBox").value; //gets the value from the textbox
 
-if (alphabet.indexOf(inputBox.value) !== -1) { //checks to see if the playuer's guess is a single letter
-    console.log("Yes")
-previous.push(inputBox.value);
+    console.log(inputBox.value) //logs the player's guess
+if (alphabet.indexOf(inputBox.value) !== -1) { //checks to see if the player's guess is a single lowercase letter
+    console.log("Yes") //logs if it is a single lowercase letter
+previous.push(inputBox.value); //pushes the player's guess to an array
 
 
 
-    guesses--
+    guesses-- //takes away a guess
 
-    if (inputBox.value === randomNum) { //checks to see if the player's guess is correct
-        document.getElementById("speechText").innerHTML = "What? No. How is this possible?"
-        console.log("You win")
-        wins++
-        restart();
+    if (inputBox.value === randomLetter) { //checks to see if the player's guess is correct
+        document.getElementById("speechText").innerHTML = "What? No. How is this possible?" //The boss is aghast!
+        console.log("You win") //logs your victory
+        wins++ //adds to wins
+        restart(); //resets the game
     }
-    else if (guesses < 1) {
-        document.getElementById("speechText").innerHTML = "You never stood a chance against MY psychic brilliance."
-        console.log("You lose")
-        losses++
-        restart();
+    else if (guesses < 1) { //checks to see if the players guess is wrong and you are out of guesses and lost the game
+        document.getElementById("speechText").innerHTML = "You never stood a chance against MY psychic brilliance." //#VillianGloating
+        console.log("You lose") //logs your utter defeat
+        losses++ //adds to your shame
+        restart(); //restarts the game
     }
-    else {
-        document.getElementById("speechText").innerHTML = "Not even close"
-        console.log("Guess again")
+    else { //checks to see if the players guess is wrong, but still has some attempts left
+        var randomQuote = evilQuote[Math.floor(Math.random() * evilQuote.length)] //selects a random villian quote
+
+        document.getElementById("speechText").innerHTML = randomQuote //displays it above the boss
+        console.log("Guess again") //logs that which you must do
     }
 
+    //logs stats
 console.log(wins)
 console.log(losses)
 console.log(guesses)
 console.log(previous)
 
+//displays stats
     document.getElementById("wins").innerHTML = "wins: " + wins;
     document.getElementById("losses").innerHTML = "losses: " + losses;
     document.getElementById("guessLeft").innerHTML = "Guesses left: " + guesses;
@@ -54,11 +60,11 @@ console.log(previous)
     
 }
 
-else if (alphabet.indexOf((inputBox.value)) === -1){
-    console.log("No")
-    document.getElementById("speechText").innerHTML = "Your guess must be a letter"
+else { //checks to see if the player's guess is not a single lowercase letter
+
+    console.log("No") //bad client, no.
+    document.getElementById("speechText").innerHTML = "Your guess must be a single, lowercase letter" //Tells you your mistake
 }
-document.getElementById('inputBox').value = "";
+
+document.getElementById('inputBox').value = ""; //clears the value in the textbox
 }
-// wins++
-// losses++
